5779451c8687740234bf88b5ac3abb6161b15342
kwe <kwevans@gmail.com>
2018-04-24 15:37:51 +0100

    hey now

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <Title />
         </header>
-        <p className="App-intro">Hey there, Tuesday</p>
+        <p className="App-intro">Hey there, Tuesday...</p>
       </div>
     );
   }
